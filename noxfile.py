0e4d13acd691f08db56ead644264d20ac664617f
Matt Mulligan <matt@mattmulligan.net>
2024-05-26 17:24:27 +1000

    update noxfile and github actions

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,11 @@
+"""Noxfile for project."""
+
 import nox
 
 
 @nox.session(name="unit-tests", python=["3.11", "3.12"])
 def unit_tests(session: nox.Session) -> None:
-    """Runs unit tests for various versions of python."""
-
+    """Run unit tests for various versions of python."""
     # install packages
     session.install("pytest", "pytest-cov", ".")
 
