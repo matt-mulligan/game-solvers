6395ebf6fcf418ec10010a61b898cd74b77712bb
Matt Mulligan <matt@mattmulligan.net>
2024-05-26 17:12:32 +1000

    add nox as dev dependency and noxfile for unit tests

---
+++ noxfile.py
@@ -0,0 +1,12 @@
+import nox
+
+
+@nox.session(name="unit-tests", python=["3.11", "3.12"])
+def unit_tests(session: nox.Session) -> None:
+    """Runs unit tests for various versions of python."""
+
+    # install packages
+    session.install("pytest", "pytest-cov", ".")
+
+    # run tests
+    session.run("pytest", "tests/unit")
